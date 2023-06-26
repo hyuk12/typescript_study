e009274c7ed4a296cde310127b3fd0608f64dda7
haehyukchoi <hotman7025@naver.com>
2023-06-26 19:33:43 +0900

    for문 별찍기 연습중
    타입스크립트로 자바로 구현했던 별찍기 구현중

---
+++ practice/src/java_to_ts/star.ts
@@ -0,0 +1,57 @@
+// public class Star {
+// 	public static void main(String[] args) {
+		
+// 		for(int i = 0; i < 10; i++) {
+// 			for(int j = 0; j < i + 1; j++) {
+// 				System.out.print("*");
+// 			}
+// 			System.out.println();
+// 		}
+
+for (let i = 0; i < 10; i++) {
+    for(let j = 0; j < i + 1; j++) {
+        console.log("*");
+    }
+    console.log("\n");
+}
+		
+// 		for(int i = 0; i < 10; i++) {
+// 			for(int j = 0; j < 10 - i; j++) {
+// 				System.out.print("*");
+// 			}
+// 			System.out.println();
+// 		}
+		
+// 		for(int i = 0; i < 10; i++) {
+// 			for(int j = 0; j < 10 - i - 1; j++) {
+// 				System.out.print(" ");
+// 			}
+// 			for(int j = 0; j < i + 1; j++) {
+// 				System.out.print("*");
+// 			}
+// 			System.out.println();
+// 		}
+		
+// 		for(int i = 0; i < 10; i++) {
+// 			for(int j = 0; j < i ; j++) {
+// 				System.out.print(" ");
+// 			}
+// 			for(int j = 0; j < 10 - i; j++) {
+// 				System.out.print("*");
+// 			}
+			
+// 			System.out.println();
+// 		}
+		
+// 		for(int i = 0; i < 10; i++) {
+// 			for(int j = 0; j < 10 - i - 1; j++) {
+// 				System.out.print(" ");
+// 			}
+// 			for(int j = 0; j < i * 2 + 1; j++) {
+// 				System.out.print("*");
+// 			}
+// 			System.out.println();
+// 		}
+// 	}
+// }
+
